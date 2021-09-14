b1d3068227057abd55d09aed14e890ec4c804781
Tarek Ziade <tarek@ziade.org>
2021-09-14 12:16:11 +0200

    added issue_repos (#1)
    new option to scan issues in other repositories that might mention a branch

--- src/actions.py
+++ src/actions.py
@@ -1,4 +1,5 @@
 from src.github import Github
+import github as pygithub
 
 
 def run_action(
@@ -6,20 +7,45 @@ def run_action(
         ignore_branches: list,
         last_commit_age_days: int,
         github_token: str,
-        dry_run: bool = True
+        dry_run: bool = True,
+        issue_repos: list = None
 ) -> list:
     input_data = {
         'github_repo': github_repo,
         'ignore_branches': ignore_branches,
         'last_commit_age_days': last_commit_age_days,
         'dry_run': dry_run,
+        'issue_repos': issue_repos
     }
 
     print(f"Starting github action to cleanup old branches. Input: {input_data}")
 
     github = Github(github_repo=github_repo, github_token=github_token)
-
     branches = github.get_deletable_branches(last_commit_age_days=last_commit_age_days, ignore_branches=ignore_branches)
+
+    # scanning for references in other repos in an open issue
+    if issue_repos is None:
+        issue_repos = []
+
+    gh = pygithub.Github(github_token)
+    issue_repos = [gh.get_repo(r) for r in issue_repos]
+
+    # loading comments for lookups
+    comments = {}
+    for issue_repo in issue_repos:
+        for issue in issue_repo.get_issues(state="open"):
+            comments[issue.html_url] = issue.body
+            for comment in issue.get_comments():
+                comments[comment.html_url] = comment.body
+
+    def keep_branch(name):
+        for url, body in comments.items():
+            if name in body:
+                print(f"`{name}` was mentioned in {url}")
+                return False
+        return True
+
+    branches = [branch for branch in branches if keep_branch(branch)]
 
     print(f"Branches queued for deletion: {branches}")
     if dry_run is False:
